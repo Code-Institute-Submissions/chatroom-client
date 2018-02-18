1fe5fc3cffcc913d110fd5a3f3cc9c318bfacc52
James Horan <horan5034@gmail.com>
2018-02-18 19:30:12 +0000

    Changed the subscription page for a singe payment scheme. Easier to implement fully functioning feature

--- public/components/subscription/controllers/subscription.controller.js
+++ public/components/subscription/controllers/subscription.controller.js
@@ -1,10 +1,10 @@
 chatroom
     .controller('SubscriptionController', function ($scope, $state, $stateParams, $cookies, $timeout, ApiService) {
         var socket;
-        
+        var user_id;
+
         function onLoad() {
             apiService = ApiService();
-            user_id = $stateParams.user_id;
             socket = io();
 
             if ($cookies.getObject('user')) {
@@ -33,68 +33,45 @@ chatroom
         };
 
         $scope.createSubscription = function (code, result) {
-            if (result.error) {
-                updateDetailsFailure();
-            } else {
-                $scope.token_id = result.id;
-                apiService.put("users/subscribe/",
-                    {
-                        'user': {
-                            'email': $scope.user.email,
-                            'is_subscribed': true,
-                            'subscription_end': Date(),
-                            'stripe_id': $scope.token_id
-                        },
-                        'stripe_token': result.id
+            $scope.token_id = result.id;
+            apiService.put("users/subscribe/",
+                {
+                    'user': {
+                        'email': $scope.user.email,
+                        'is_subscribed': true,
+                        'subscription_end': Date(),
                     },
-                    subscribeSuccess,
-                    subscribeFailure)
-            }
+                    'stripe_token': result.id
+                },
+                subscribeSuccess,
+                subscribeFailure)
         };
 
         function subscribeSuccess(response) {
             $scope.user = response.data;
             $cookies.putObject('user', $scope.user);
-            $scope.croppedProfileImage = "";
-            $scope.picFile = "";
 
             $('#subscription-message').addClass('alert-success').removeClass('hide');
-            $scope.subscription_message = "Subscription Created Successfully!";
+            $scope.subscriptionMessage = "Subscription Created Successfully!";
 
             setTimeout(function () {
                 $timeout(function () {
-                    $scope.subscription_message = "";
-                    $scope.user.is_subscribed = true;
+                    $scope.subscriptionMessage = "";
+                    $('#subscription-message').removeClass('alert-success').addClass('hide');
                 });
-                $('#subscription-message').removeClass('alert-success').addClass('hide');
             }, 2000);
         };
 
         function subscribeFailure(response) {
             $('#subscription-message').addClass('alert-danger').removeClass('hide');
-            $scope.message = "Failed to to make payment this time!";
+            $scope.subscriptionMessage = response.data;
             setTimeout(function () {
                 $timeout(function () {
-                    $scope.message = "";
+                    $scope.subscriptionMessage = "";
                 });
                 $('#subscription-message').removeClass('alert-danger').addClass('hide');
             }, 2000);
         };
-
-        $scope.cancelSubscription = function () {
-            apiService.post('users/cancel_subscription/' +  $scope.user.id + '/',
-                null,
-                cancelSuccess,
-                cancelFailure)
-        }
-
-        function cancelSuccess(response) {
-            
-        }
-
-        function cancelFailure(response) {
-            
-        }
 
         onLoad();
     });
